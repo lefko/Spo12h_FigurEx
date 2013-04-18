3d8d2f4a3965979d6ced1ea4280ad484fbbe40c9
Andreas Lefkopoulos <andreas.l@adb.se>
2013-04-18 11:24:32 +0200

    Added class Point.cs

---
+++ FigurEx/FigurEx/Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FigurEx
+{
+    class Point : IMoveable
+    {
+        private int x;
+        private int y;
+
+        public Point():this(10,10)
+        {}
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X
+        {
+            get { return x;  }
+            set
+            {
+                if ( value >= 0)
+                    x = value;
+            }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set
+            {
+                if (value >= 0)
+                    y = value;
+            }
+        }
+
+        public void Move(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
